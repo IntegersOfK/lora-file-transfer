5e888c4b489df7e2c23dd1ff5a36fb1fa222461f
AJ West <ajwest@gmail.com>
2020-07-02 10:43:31 -0400

    Refactoring

--- run.py
+++ run.py
@@ -1,93 +1,30 @@
 
 import os
-import board
-import busio
-import digitalio
 import argparse
 import pathlib
 import textwrap
 import time
 import hashlib
 import json
-import  progressbar
+import progressbar
+import setupbon # offload all the bonnet-specific settings
 
-# Import the SSD1306 module.
-import adafruit_ssd1306
-import adafruit_rfm9x
 
-# Button A
-btnA = digitalio.DigitalInOut(board.D5)
-btnA.direction = digitalio.Direction.INPUT
-btnA.pull = digitalio.Pull.UP
-
-# Button B
-btnB = digitalio.DigitalInOut(board.D6)
-btnB.direction = digitalio.Direction.INPUT
-btnB.pull = digitalio.Pull.UP
-
-# Button C
-btnC = digitalio.DigitalInOut(board.D12)
-btnC.direction = digitalio.Direction.INPUT
-btnC.pull = digitalio.Pull.UP
-
-# Create the I2C interface.
-i2c = busio.I2C(board.SCL, board.SDA)
-
-# 128x32 OLED Display
-reset_pin = digitalio.DigitalInOut(board.D4)
-display = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c, reset=reset_pin)
-# Clear the display.
-display.fill(0)
-display.show()
-width = display.width
-height = display.height
-
-# set the min time interval (in seconds) to wait between sending packets
-min_transmit_interval = 0
-
-# Define radio parameters.
-RADIO_FREQ_MHZ = 915.0  # Frequency of the radio in Mhz. Must match your
-# module! Can be a value like 915.0, 433.0, etc.
-
-# Define pins connected to the chip.
-CS = digitalio.DigitalInOut(board.CE1)
-RESET = digitalio.DigitalInOut(board.D25)
-
-# Initialize SPI bus.
-spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
-
-# Initialze RFM radio
-
-# Attempt to set up the rfm9x Module
-try:
-    rfm9x = adafruit_rfm9x.RFM9x(spi, CS, RESET, RADIO_FREQ_MHZ)
-    display.text("rfm9x: Detected", 0, 0, 1)
-except RuntimeError:
-    # Thrown on version mismatch
-    display.text("rfm9x: ERROR", 0, 0, 1)
-
-display.show()
-
-# enable CRC checking
-rfm9x.enable_crc = True
-
-#rfm9x.ack_wait(5.0) # increase the acknowledgement wait to 5 seconds from 0.5)
-#rfm9x.receive_timeout(5.0) # increase the recieve timeout to 5 seconds.. might not be needed and could cause issues for button detection?
-
-class BonnetInteract():
-    listen = False
+class Transceiver():
     #bytes_per_message = 252 # this is the max number of bytes the adafruit_rfm9x library is able to send in a message over LoRa
     bytes_per_message = 100
-    fernet = None
-    display = None
-    send_or_rec = ['recieve', 'send']
+    send_or_rec = 'recieve'
     valid_modes = ['data destination node id', 'this node id', 'toggle send or recieve']
     selection_mode = valid_modes[0]
-    rfm9x = None
+    min_transmit_interval = 0
+    packaged_data = {} # where we store files ready to be sent
+    collected = {}  # where we store recieved messages until we have all pieces
 
-    def __init__(self, rfm9x, display, fernet=None):
+    def __init__(self, rfm9x, display, incoming, outgoing=None, fernet=None):
         self.rfm9x = rfm9x
         self.display = display
+        self.incoming_directory = incoming
+        self.outgoing_file_path = outgoing
         self.fernet = fernet
 
     def update_display(self, text):
@@ -115,15 +52,13 @@ class BonnetInteract():
                 self.rfm9x.node = 255
             self.update_display("This node id is set to: {0}".format(self.rfm9x.node))
         elif self.selection_mode == self.valid_modes[2]:
-            self.listen = False
             if self.send_or_rec == 'receive':
                 self.send_or_rec = 'send'
             else:
                 self.send_or_rec = 'receive'
-                self.listen = True
             self.update_display("This node is configured to: {0} press button 3 to start".format(self.send_or_rec.upper()))
 
-    def increase(self, fernet=None):
+    def increase(self):
         """Third button. Changes the destination or node number to up one"""
         if self.selection_mode == self.valid_modes[0]:
             if self.rfm9x.destination < 255:
@@ -140,116 +75,140 @@ class BonnetInteract():
         elif self.selection_mode == self.valid_modes[2]:
             if self.send_or_rec == 'receive':
                 self.update_display("Receive mode listening for messages...")
-                self.listen = True
             else:
                 self.update_display("Send mode, sending requested file...")
-                self.listen = False
                 self.send()
-    
-    def send(self):
+
+
+    def request_file_metadata(self):
+        """When somebody sends a message requesting a file"""
         all_bytes = pathlib.Path(self.outgoing_file_path).read_bytes() # get the file as bytes
-        filehash = hashlib.sha256(all_bytes).hexdigest()[:10] # we can reasonably assume the first 10 characters are good enough to know it's the right file
+        # we can reasonably assume the first 6 characters are good enough to know it's the right file. Sure, small chance of collision but it will be used in every message so it needs to be short!
+        filehash = hashlib.sha256(all_bytes).hexdigest()[:6]
         if self.fernet:
             print("Encrypting bytes string, size before encryption " + str(len(all_bytes)))
             all_bytes = self.fernet.encrypt(all_bytes)
             print("Size after encryption " + str(len(all_bytes)))
-        packaged_data = [bytearray(all_bytes[i:i+self.bytes_per_message]) for i in range(0, len(all_bytes), self.bytes_per_message)] # turn it into the right number of messages
+        self.packaged_data[filehash] = [bytearray(all_bytes[i:i+self.bytes_per_message], 'utf-8') for i in range(0, len(all_bytes), self.bytes_per_message)] # turn it into the right number of messages
 
         print(str(self.outgoing_file_path) + ' is ' + str(len(all_bytes)/1024)  +' kilobytes and can be sent in ' + str(len(packaged_data)) + ' messages of ' + str(self.bytes_per_message) + ' bytes each')
 
         start_time = time.time()
         print('Sending metadata about the file we are about to send')
-        metadata = [0, pathlib.Path(self.outgoing_file_path).name, len(packaged_data), filehash] # status 0 to start, filename, number of messages to be sent, hash
-        print(metadata)
+        metadata = [0, filehash, pathlib.Path(self.outgoing_file_path).name, len(packaged_data)] # status 0 to start, filehash, filename, number of messages to be sent
         encoded_metadata = bytearray(json.dumps(metadata), 'utf-8')
         print(encoded_metadata)
         self.rfm9x.send_with_ack(encoded_metadata)
 
-        for p in progressbar.progressbar(packaged_data, redirect_stdout=True):
-            print(p)
-            if min_transmit_interval: time.sleep(min_transmit_interval)
-            self.rfm9x.send_with_ack(p)
-        self.rfm9x.send_with_ack(bytearray(json.dumps([1, filehash]), 'utf-8')) # confirm all the pieces were sent, status 1 to end
-
-        print('Done! Transmit took ' + str(int(time.time()-start_time)) + ' seconds')
-
-
-class Receiver():
-
-    message_count = 0
-    collected = []  # where we store the messages until we have all pieces
-    filename = None
-    total_size = None
-    total_messages = None
-    filehash = None
-    output_dir = None
-    fernet = None
-    b = None
-
-    def __init__(self, output_dir, bon):
-        self.output_dir = output_dir
-        self.b = bon
-
-    def combine_pieces(self):
+    def send_pieces(self, filehash, part):
+        """Sends piece(s) of the requested file"""
+        if part == 0:
+            # send all parts in sequence
+            index = 0
+            for p in self.packaged_data[filehash]['messages']:
+                 #if self.min_transmit_interval: time.sleep(self.min_transmit_interval)
+                self.rfm9x.send_with_ack(bytes(str(index).zfill(4)) + bytes(filehash, 'utf-8') + p) # hash is the delimiter between metadata and filedata.
+        else:
+            # send the specific part requested
+            self.rfm9x.send_with_ack(bytes(str(index).zfill(4)) + bytes(filehash, 'utf-8') + self.packaged_data[filehash][part]) # hash is the delimiter
+        
+    def combine_pieces(self, filehash):
         """Puts together all the pieces in the list"""
-        all_bytes = b''.join(self.collected)
+        all_bytes = b''.join(self.collected[filehash])
         if self.fernet:
             all_bytes = f.decrypt(all_bytes)
             print("Decrypted data with provided password")
-        filehash = hashlib.sha256(all_bytes).hexdigest()[:10]
-        if filehash == self.filehash:
+        this_filehash = hashlib.sha256(all_bytes).hexdigest()[:6]
+        if this_filehash == filehash:
             print("File hash matches, file integrity confirmed!")
         else:
             print("File hash doesn't match, writing to file anyway...")
-        with open(os.path.join(self.output_dir, self.filename), "wb") as f:
+        with open(os.path.join(self.output_dir, self.collected[filehash]['filename']), "wb") as f:
             f.write(all_bytes)
-        print("Done, got the file! " + os.path.join(self.output_dir, self.filename))
+        print("Done, got the file! " + os.path.join(self.output_dir, self.collected[filehash]['filename']))
 
-        # reset everything
-        self.message_count = 0
-        self.collected = []
-        self.filename = None
-        self.total_messages = None
-        self.filehash = None
     
+    # These are the codes we use in the first 4 bytes to know what we want
+    CODES = {'-200'}, # request a list of file hashes
+            '-201': {'h':None}}, # request a file's metadata
+            '-202', {'h':None}} # request all parts of a file
+            '-203', {'h':None, 'p':0}, # request specific part of a file
+            '-301' # make and forward command functions (with pieces?)
+            }                   
+                                
+
     def process_message(self, packet):
-        """Deals with the collection of packets and to stick them pack together"""
+        """Deals with requests for information OR the collection of packets to stick them pack together when we have them all"""
+        # TODO Working HERE do the check for data
         # Check if there's metadata
-        try:
-            decoded = packet.decode('utf-8')
-            print(decoded)
-            a = json.loads(decoded)
-            if len(a):
-                status = int(a[0])
-                if status == 0:
-                    self.filename = a[1]
-                    self.total_messages = a[2]
-                    self.filehash = a[3]
-                    print("Recieved metadata about a new file to receive.")
-                    self.b.update_display("File incoming... " + self.filename)
-                    self.message_count = 0
-                    collected = []
-                if status == 1:
-                    print("Recieved all messages to recreate the file")
-                    self.b.update_display("File done recieving! " + self.filename)
-                    self.combine_pieces()
-        except Exception as e: # for cases when it's not metadata...
-            if not self.filehash:
-                print("Packet was received but no metadata was received earlier, ignoring")
-                self.b.update_display("Got packet, but no metadata")
-                return
-            self.message_count += 1
-            self.collected.append(bytes(packet))
-            print("Recieving packet " + str(self.message_count) + " of " +  str(self.total_messages))
-            self.b.update_display("Recieving packet " + str(self.message_count) + " of " +  str(self.total_messages))
+        # We need to always know that it's a bytesarray... need to find away against no json differentiation
+        # check if request for metafdaa
+
+        # We use the first 10 bytes in the packet The first 4 are piece numbers, and the next 6 are hash
+        piece = packet[:4]
+        filehash = packet[4:10]
+        data = packet[10:]
+        #print(piece)
+        # if not self.collected.get(filehash):
+        #     print("A file we have not seen before! " + filehash)
+        #     self.collected[filehash] = {'meta':{}, 'messages':{}}
+
+        # if piece == b'-200':
+        #     print("A specific piece of a file is being requested")
+        #     self.collected[filehash]['filename']
+        #     a = json.loads(data.decode('utf-8'))
+
+        # elif piece == b'-101':
+        #     print("Meta information is being requested about the file to send!")
+        #     self.rfm9x.send_with_ack(bytes(str(index).zfill(4)) + bytes(filehash, 'utf-8') + self.packaged_data[filehash][part]) # hash is the delimiter
+
+        # elif piece == b'-102':
+        #     print("Meta information is arriving about the file we want!")
+        #     self.collected[filehash]['filename'] = a[0]
+        #     a = json.loads(data.decode('utf-8'))
+
+
+        # elif piece.isdigit():
+        #     print("A piece of a file is being requested")
+        #     self.rfm9x.send_with_ack(bytes(str(index).zfill(4)) + bytes(filehash, 'utf-8') + self.packaged_data[filehash][part]) # hash is the delimiter
+        #     self.collected[filehash]['messages'][int(piece)] = data
+
+        
+        # try:
+        #     decoded = packet.decode('utf-8')
+        #     print(decoded)
+        #     a = json.loads(decoded)
+        #     if len(a):
+        #         status = int(a[0])
+        #         if status == 0:
+        #             self.filename = a[1]
+        #             self.total_messages = a[2]
+        #             self.filehash = a[3]
+        #             print("Recieved metadata about a new file to receive.")
+        #             self.b.update_display("File incoming... " + self.filename)
+        #             self.message_count = 0
+                    
+        #         if status == 1:
+        #             print("Recieved all messages to recreate the file")
+        #             self.b.update_display("File done recieving! " + self.filename)
+        #             self.combine_pieces()
+        # except Exception as e: # for cases when it's not metadata...
+        #     if not self.filehash:
+        #         print("Packet was received but no metadata was received earlier, ignoring")
+        #         self.b.update_display("Got packet, but no metadata")
+        #         return
+        #     self.message_count += 1
+        #     self.collected.append(bytes(packet))
+        #     print("Recieving packet " + str(self.message_count) + " of " +  str(self.total_messages))
+        #     self.b.update_display("Recieving packet " + str(self.message_count) + " of " +  str(self.total_messages))
 
 def main(b):
     last_press = time.time()
     button_debounce = 0.200 # time until another press can be registered
     while True:
-        if True:
+        if b.send_or_rec == 'receive':
             # Look for a new packet: only accept if addresses to my_node
-            packet = rfm9x.receive(keep_listening=True, with_header=False, with_ack=True, timeout=0.3)
+            packet = b.rfm9x.receive(keep_listening=True, with_header=False, with_ack=True, timeout=0.3)
             # If no packet was received during the timeout then None is returned.
             if packet is not None:
                 # Received a packet!
@@ -259,7 +218,7 @@ def main(b):
                 print("Received RSSI: {0}".format(rfm9x.last_rssi))
                 b.update_display("Recieving packet! Time: " + str(time.time()))
                 print("Receiving packet! Time: " + str(time.time()))
-                r.process_message(packet)
+                b.process_message(packet)
                 packet = None
         if last_press < time.time()-button_debounce:
             last_press = time.time()
@@ -296,12 +255,10 @@ if __name__ == '__main__':
         fernet = Fernet(key)
         print("Password encryption enabled")
 
-    b = BonnetInteract(rfm9x, display, fernet)
-    r = Receiver(args.incoming, b)
-    if args.outgoing:
-        b.outgoing_file_path = args.outgoing
-
+    rfm9x, display = setupbon.setupbon()
     # start main loop to check for messages and button presses
-    main(b)
+    main(Transceiver(rfm9x, display, args.incoming, args.outgoing, fernet))
+
+    
 
 
---
+++ setupbon.py
@@ -0,0 +1,68 @@
+
+
+# Import the SSD1306 module.
+import adafruit_ssd1306
+import adafruit_rfm9x
+import board
+import busio
+import digitalio
+
+def setup_bonnet():
+
+    # Button A
+    btnA = digitalio.DigitalInOut(board.D5)
+    btnA.direction = digitalio.Direction.INPUT
+    btnA.pull = digitalio.Pull.UP
+
+    # Button B
+    btnB = digitalio.DigitalInOut(board.D6)
+    btnB.direction = digitalio.Direction.INPUT
+    btnB.pull = digitalio.Pull.UP
+
+    # Button C
+    btnC = digitalio.DigitalInOut(board.D12)
+    btnC.direction = digitalio.Direction.INPUT
+    btnC.pull = digitalio.Pull.UP
+
+    # Create the I2C interface.
+    i2c = busio.I2C(board.SCL, board.SDA)
+
+    # 128x32 OLED Display
+    reset_pin = digitalio.DigitalInOut(board.D4)
+    display = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c, reset=reset_pin)
+    # Clear the display.
+    display.fill(0)
+    display.show()
+
+    # # set the min time interval (in seconds) to wait between sending packets
+    # min_transmit_interval = 0
+
+    # Define radio parameters.
+    RADIO_FREQ_MHZ = 915.0  # Frequency of the radio in Mhz. Must match your
+    # module! Can be a value like 915.0, 433.0, etc.
+
+    # Define pins connected to the chip.
+    CS = digitalio.DigitalInOut(board.CE1)
+    RESET = digitalio.DigitalInOut(board.D25)
+
+    # Initialize SPI bus.
+    spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
+
+    # Initialze RFM radio
+
+    # Attempt to set up the rfm9x Module
+    try:
+        rfm9x = adafruit_rfm9x.RFM9x(spi, CS, RESET, RADIO_FREQ_MHZ)
+        display.text("rfm9x: Detected", 0, 0, 1)
+    except RuntimeError:
+        # Thrown on version mismatch
+        display.text("rfm9x: ERROR", 0, 0, 1)
+
+    display.show()
+
+    # enable CRC checking
+    rfm9x.enable_crc = True
+    return rfm9x, display
+
+    #rfm9x.ack_wait(5.0) # increase the acknowledgement wait to 5 seconds from 0.5)
+    #rfm9x.receive_timeout(5.0) # increase the recieve timeout to 5 seconds.. might not be needed and could cause issues for button detection?
